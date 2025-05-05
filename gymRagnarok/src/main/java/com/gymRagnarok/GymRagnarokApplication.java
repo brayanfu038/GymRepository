3e9986ecb590ca6a64029d8e4c198fa50e2c8ed9
Brayan <brayan.fuquene01@uptc.edu.co>
2025-05-04 22:35:45 -0500

    se agrego la estructura del proyecto

---
+++ gymRagnarok/src/main/java/com/gymRagnarok/GymRagnarokApplication.java
@@ -0,0 +1,13 @@
+package com.gymRagnarok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymRagnarokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymRagnarokApplication.class, args);
+	}
+
+}
---
+++ gymRagnarok/src/test/java/com/gymRagnarok/GymRagnarokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymRagnarok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymRagnarokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
