adc29333c5b463115c867a2f3fd7ef139b38befd
Kath1109 <katherine1109bell@gmail.com>
2025-05-05 17:52:52 -0500

    Login terminado

--- frontend-gym/src/Components/Login.tsx
+++ frontend-gym/src/Components/Login.tsx
@@ -8,10 +8,17 @@ const Login: React.FC = () => {
     <div className='divContLeft'>
       <h1>Ragnarok</h1>
       <h2>Lideramos el cambio, forjamos campeones.</h2>
-      <p>Domina la gestión con herramientas que impulsan tu éxito cada día</p>
+      <p className='text'>Domina la gestión con herramientas que impulsan tu éxito cada día</p>
     </div>
   </div>
   <div className='divRight'>
+    <h3>Bienvenido a Ragnarok Gym</h3>
+    <p className='txt'>Ingresa tus credenciales para continuar</p>
+    <form className='formulario' action="">
+      <input className='input' type="text" placeholder='Usuario'/>
+      <input className='input' type="password" placeholder='Contraseña' />
+      <input className='input' id='sendLogin' type="submit" />
+    </form>
   </div>
 </div>
     
