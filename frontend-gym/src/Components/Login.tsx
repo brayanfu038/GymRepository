86bd4da9611335a31140f76b7f8c2cf0181edd33
Kath1109 <katherine1109bell@gmail.com>
2025-05-05 16:44:57 -0500

    Divs de login

---
+++ frontend-gym/src/Components/Login.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+
+const Login: React.FC = () => {
+  return (
+<div className='main'>
+  <div className='divLeft'>
+    <div className='divContLeft'>
+      <h1>Ragnarok</h1>
+      <h2>Lideramos el cambio, forjamos campeones.</h2>
+      <p>Domina la gestión con herramientas que impulsan tu éxito cada día</p>
+    </div>
+  </div>
+  <div className='divRight'>
+  </div>
+</div>
+    
+  );
+};
+
+export default Login;
