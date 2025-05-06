51fcac69b9f544ec7ba415892f01ad496ebe87ec
Kath1109 <katherine1109bell@gmail.com>
2025-05-06 09:06:51 -0500

    Ultimos cambios

--- frontend-gym/src/App.tsx
+++ frontend-gym/src/App.tsx
@@ -1,20 +1,18 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Login from './Components/Login';
-// import DashboardA from './Components/DashboardA';
-// import TopBar from './Components/TopBar';
-// import NuevoUsuario from './Components/NuevoUsuario';
-import DashboardS from "./Components/DashboardS";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DashboardA from './Components/DashboardA';
+// import DashboardS from './Components/DashboardS';
+import Login from './Components/Login';
+
 
 function App() {
   return (
-<DashboardS/>
-
-  //   <Router>
-  //   <Routes>
-  //     <Route path = '/' element = {<Login />} />
-  //     <Route path = '/dashboard' element = {<DashboardA />} />
-  //   </Routes>
-  // </Router>
+  // <DashboardS/>
+    <Router>
+    <Routes>
+      <Route path = '/' element = {<Login />} />
+      <Route path = '/dashboard' element = {<DashboardA />} />
+    </Routes>
+  </Router>
       );
 
 
