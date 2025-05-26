e476f20354174b015e25aa2331d1e0736f43683b
Jesus Fernando Garzon Garcia <81579165+JesusFernandoGarzonGarcia@users.noreply.github.com>
2025-05-26 10:43:33 -0500

    correccion de visualizacion de sidemenu

--- frontend-gym/src/Components/generals/SideMenu.tsx
+++ frontend-gym/src/Components/generals/SideMenu.tsx
@@ -1,34 +1,52 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./SideMenu.css";
 import { FiUsers } from "react-icons/fi";
 import { MdOutlineInventory } from "react-icons/md";
-import { FaCalendarAlt } from "react-icons/fa";
-import { FaChartLine } from "react-icons/fa";
+import { FaCalendarAlt, FaChartLine } from "react-icons/fa";
 import { FaRightFromBracket } from "react-icons/fa6";
-
-
-
 
 const SideMenu: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Función auxiliar para saber si una ruta está activa
+  const isActive = (ruta: string) => location.pathname === ruta;
+
   return (
     <div className="leftMenu">
-      <h1>Menú principal</h1>
-      <button className="menu-btn" onClick={() => navigate('/membresias')}>
-      <FiUsers size={24} /> Gestionar Membresías
+      <div className="titulo">
+      <h1>MENÚ PRINCIPAL</h1>
+      </div>
+      <button
+        className={`menu-btn ${isActive('/membresias') ? 'active' : ''}`}
+        onClick={() => navigate('/membresias')}
+      >
+        <FiUsers size={24} /> Gestionar Membresías
       </button>
-      <button className="menu-btn" onClick={() => navigate('/usuarios')}>
-      <FaCalendarAlt size={24} /> Gestionar Usuarios
+      <button
+        className={`menu-btn ${isActive('/usuarios') ? 'active' : ''}`}
+        onClick={() => navigate('/usuarios')}
+      >
+        <FaCalendarAlt size={24} /> Gestionar Usuarios
       </button>
-      <button className="menu-btn" onClick={() => navigate('/Inventario')}>
-      <MdOutlineInventory size={24} /> Gestionar Inventarios
+      <button
+        className={`menu-btn ${isActive('/inventario') ? 'active' : ''}`}
+        onClick={() => navigate('/inventario')}
+      >
+        <MdOutlineInventory size={24} /> Gestionar Inventarios
       </button>
-      <button className="menu-btn" onClick={() => navigate('/usuarios')}>
-      <FaChartLine size={24} /> Información Financiera
+      <button
+        className={`menu-btn ${isActive('/finanzas') ? 'active' : ''}`}
+        onClick={() => navigate('/finanzas')}
+      >
+        <FaChartLine size={24} /> Información Financiera
       </button>
-      <button className="menu-btn" onClick={() => navigate('/usuarios')}>
-      <FaRightFromBracket size={24}/> Log Out
+      <button
+        className={`menu-btn ${isActive('/logout') ? 'active' : ''}`}
+        onClick={() => navigate('/logout')}
+      >
+        <FaRightFromBracket size={24} /> Log Out
       </button>
     </div>
   );
